0b4538dc3fe58e98f1f312bb22f12ba24719763b
YoungsonMan <choi13570@naver.com>
2025-04-01 16:32:00 +0900

    [Chores]: GitIgnore 수정
    GitIgnore에 imports폴더 추가

--- Assets/01Scripts/Player/PlayerController.cs
+++ Assets/01Scripts/Player/PlayerController.cs
@@ -44,23 +44,29 @@ public class PlayerController : MonoBehaviour
         // 방향키만 입력받고 업데이트에서 계속 굴리기
         Vector2 input = value.Get<Vector2>();
         
-        _moveDirection = new Vector3(input.x, 0, input.y).normalized;
+        _moveDirection = new Vector3(input.x, 0, input.y);
         // RigidBody로 이동
-        Debug.Log( $"방향키입력받음. moveDir: {_moveDirection},  inputMagnitude: {input.magnitude}");
+        // Debug.Log( $"방향키입력받음. moveDir: {_moveDirection},  inputMagnitude: {input.magnitude}");
         // TODO: Animation View 세팅 
         
         
     }
     public void Move()
     {
+        
         _rigidBody.velocity = _moveDirection.normalized * _playerModel.speed; 
+        Debug.Log( $"방향키입력받음. moveDir: {_moveDirection},  inputMagnitude: {_moveDirection.magnitude}");
         // 계속움직임 방지
         if (_moveDirection.magnitude < 0.1f)
         {
             _rigidBody.velocity = Vector2.zero;
         }
         // 마지막 방향 유지
-        if(_moveDirection.magnitude > 0.1f) transform.forward = _moveDirection.normalized;
+        // transform.forward = _moveDirection.normalized;
+        if(_moveDirection.magnitude > 1f) transform.forward = _moveDirection.normalized;
+        
+        // 0,0,0
+        
 
     }
     public void OnDodge(InputValue value)
