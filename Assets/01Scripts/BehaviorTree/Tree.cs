5d2ff7c3028e9aa05ac6327e59af470e7c0b2eb0
YoungsonMan <choi13570@naver.com>
2025-04-07 22:27:58 +0900

    [Feat]: Sequence Node 추가

--- Assets/01Scripts/BehaviorTree/Tree.cs
+++ Assets/01Scripts/BehaviorTree/Tree.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 
 namespace BehaviorTree
 {
-    public class Tree : MonoBehaviour
+    public abstract class Tree : MonoBehaviour
     {
-        
+        private Node _root = null; // recursively contains the entire tree
+
+        // start에서 셋업하고
+        protected void Start()
+        {
+            _root = SetupTree();
+        }
+
+        // update에서 돌면서 evaluate
+        private void Update()
+        {
+            if (_root == null) _root.Evaluate();
+        }
+
+        protected abstract Node SetupTree();
     }
     
 }
