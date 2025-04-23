6b8d04927a1c47748d9a40156fe2e74398262fd4
YoungsonMan <choi13570@naver.com>
2025-04-24 00:17:58 +0900

    [Fix]: 몬스터Node_타겟추적 수정
    거리연산 로직 잘못 돼었던것 수정
    
    노드상태 잘 못되어있던것 수정

--- Assets/01Scripts/MonsterAI/TaskGoToTarget.cs
+++ Assets/01Scripts/MonsterAI/TaskGoToTarget.cs
@@ -9,22 +9,24 @@ public class TaskGoToTarget : Node
 
     public TaskGoToTarget(Transform transform)
     {
-        _transform = transform;
+        _transform = transform; // 자신의 transform 가져오기
     }
 
     public override NodeState Evaluate()
     {
         Transform target = (Transform)GetData("target");
+        Debug.Log($"몬스터Node_타겟추격 : 타겟설정 = {target} ");
+        Debug.Log($"몬스터Node_타겟추격 : TargetPoisition = {target.position} ");
 
-        if (Vector3.Distance(_transform.position, target.position) < 0.1f)
+        if (Vector3.Distance(_transform.position, target.position) > 0.1f) // 지정 거리보다 멀면 접근
         {
             _transform.position = Vector3.MoveTowards(_transform.position, target.position, 0.1f); // target.posiotn, 다음에 몹속도 * Time.deltaTime
-            _transform.LookAt(target.position);
-            Debug.Log($"몬스터Node_타겟접근 : 범위에 들어옴 targetPosition = {target.position} ");
+            _transform.LookAt(target.position);         // 타겟 바라보기
+            Debug.Log($"몬스터Node_타겟추격 : 범위에 들어옴 targetPosition = {target.position} ");
         }
 
-        state = NodeState.SUCCESS;
-        Debug.Log("몬스터Node_타겟접근 : SUCCESS");
+        state = NodeState.RUNNING;
+        Debug.Log("몬스터Node_타겟추격 : RUNNING");
         return state;
     }
 }
